2e5b5f72f889a3e350bbbe02c501a4ec4c8f201f
Sharapatov Nikita <neffshar@gmail.com>
2023-10-06 11:28:09 +0300

    create migrations

--- web_courseworks/push_stud/models.py
+++ web_courseworks/push_stud/models.py
@@ -1,19 +1,20 @@
 from django.db import models
+import uuid
 
 
 class Groups(models.Model):
-    group_name = models.CharField(max_length=10)
+    group_name = models.CharField('Группа',max_length=10)
 
 
 class Students(models.Model):
     group_id = models.ForeignKey(Groups, on_delete=models.CASCADE)
-    student_url = models.CharField(max_length=100)
-    student_first_name = models.CharField(max_length=20)
-    student_name = models.CharField(max_length=20)
-    student_second_name = models.CharField(max_length=20)
+    student_first_name = models.CharField('Фамилия', max_length=20)
+    student_name = models.CharField('Имя', max_length=20)
+    student_second_name = models.CharField('Отчество', max_length=20)
+    students_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
 
 class Portfolio(models.Model):
-    student_id = models.ForeignKey(Groups, on_delete=models.CASCADE)
-    subject = models.CharField(max_length=100)
-    title = models.CharField(max_length=150)
+    subject = models.TextField(max_length=100)
+    title = models.TextField(max_length=250)
+    student_uuid = models.ForeignKey(Students, on_delete=models.CASCADE)
